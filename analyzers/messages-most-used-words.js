17ed39116fd774b88ddb71eb49da0492f5f4c523
verdie-g <gregoire.verdier@gmail.com>
2019-03-16 13:00:40 +0100

    Simplify most words used analyzer

--- analyzers/messages-most-used-words.js
+++ analyzers/messages-most-used-words.js
@@ -4,21 +4,14 @@ const { unaccent } = require('../helpers/string');
 const MIN_CHAR = 3;
 
 module.exports = (ctx, data, out) => {
-  const wordsUsed = data.messages
+  data.messages
     .filter(message => message.sender_name === ctx.fullName)
-    .reduce((wordsUsed, message) => {
-      message.content
-        .split(/[\s.()[\]"'`\-,;:/!?]+/)
+    .forEach((message) => {
+      message.content.split(/[\s.()[\]"'`\-,;:/!?]+/)
         .filter(word => word.length >= MIN_CHAR)
         .forEach((word) => {
           const formattedWord = unaccent(word.toLowerCase());
-          addOrDefault(wordsUsed, formattedWord, 1, 1);
+          addOrDefault(out, formattedWord, 1, 1);
         });
-      return wordsUsed;
-    }, {});
-
-  Object.entries(wordsUsed)
-    .map(([word, count]) => ({ word, count }))
-    .sort(obj => obj.count)
-    .forEach(obj => addOrDefault(out, obj.word, obj.count, 1));
+    });
 };
